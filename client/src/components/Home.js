37af3754d87124a9ee74178d3a18600a6e902b9c
Marc Cavazos <marcavazos19@icloud.com>
2022-03-30 11:33:22 -0500

    carousel

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import MeetUps from './MeetUps';
 
 function Home() {
   return (
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Login() {
+function Login({ setUser }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/login", {
+      method: "POST",
+      headers: { 
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }).then(r => {
+      if (r.ok) {
+        r.json().then(user => setUser(user));
+      }
+    });
+  }
+
   return (
-    <div>Login</div>
+    <Container>
+      
+      <Form onSubmit={handleSubmit}>
+
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email"
+           onChange={e => setUsername(e.target.value)}/>
+        </Form.Group>
+
+        <Form.Group class="mb-3" controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" 
+          onChange={e => setPassword(e.target.value)}/>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formCheckbox">
+          <Form.Check type="checkbox" label="Send me promotional info and offers" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   )
 }
 
